5716d0c614f0b6e7caa6c31b5702d6123ac51274
Jiahao Li <lijiahao90@gmail.com>
2011-11-02 12:18:24 -0700

    first commit

---
+++ src/edu/cs160/main/Cs160projectActivity.java
@@ -0,0 +1,13 @@
+package edu.cs160.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Cs160projectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
